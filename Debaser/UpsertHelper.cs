0add71efbfbed7d37866aa665747de24a44c759c
Mogens Heller Grabe <mookid8000@gmail.com>
2016-11-22 21:58:06 +0100

    schema things

--- Debaser/UpsertHelper.cs
+++ Debaser/UpsertHelper.cs
@@ -1,29 +1,52 @@
 ﻿using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using System.Threading.Tasks;
+using Debaser.Mapping;
+using Debaser.Schema;
 
 namespace Debaser
 {
     public class UpsertHelper<T>
     {
+        readonly string _schema;
+        readonly string _tableName;
         readonly string _connectionString;
+        readonly ClassMap _classMap;
+        SchemaCreator _schemaCreator;
 
-        public UpsertHelper(string connectionStringOrConnectionStringName)
+        public UpsertHelper(string connectionStringOrConnectionStringName, string tableName = null, string schema = "dbo")
         {
             var connectionStringSettings = ConfigurationManager.ConnectionStrings[connectionStringOrConnectionStringName];
 
             _connectionString = connectionStringSettings?.ConnectionString
                                 ?? connectionStringOrConnectionStringName;
+
+            _tableName = tableName ?? typeof(T).Name;
+            _schema = schema;
+
+            _classMap = new AutoMapper().GetMap(typeof(T));
+
+            _schemaCreator = GetSchemaCreator();
         }
 
         public void DropSchema()
         {
-            
+            _schemaCreator.DropSchema();
         }
 
         public void CreateSchema()
         {
-            
+            _schemaCreator.CreateSchema();
+        }
+
+        SchemaCreator GetSchemaCreator()
+        {
+            var properties = _classMap.Properties.ToList();
+            var dataTypeName = $"{_tableName}Type";
+            var procedureName = $"{_tableName}Upsert";
+            var keyProperties = properties.Where(p => p.IsKey);
+            return new SchemaCreator(_connectionString, _tableName, dataTypeName, procedureName, keyProperties, properties, _schema);
         }
 
         public async Task Upsert(IEnumerable<T> rows)
