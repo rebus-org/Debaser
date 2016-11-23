bfdb91f32c249d13c41ad72abbf0a1dac3af6623
Mogens Heller Grabe <mookid8000@gmail.com>
2016-11-23 12:34:43 +0100

    auto-create database

--- Debaser.Tests/FixtureBase.cs
+++ Debaser.Tests/FixtureBase.cs
@@ -1,11 +1,18 @@
 ﻿using System.Configuration;
+using System.Data.SqlClient;
 using NUnit.Framework;
 
 namespace Debaser.Tests
 {
     public abstract class FixtureBase
     {
+        const int DatabaseAlreadyExists = 1801;
         protected static string ConnectionString => ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+
+        static FixtureBase()
+        {
+            EnsureDatabaseExists("debaser_test");
+        }
 
         [SetUp]
         public void InnerSetUp()
@@ -15,7 +22,29 @@ namespace Debaser.Tests
 
         protected virtual void SetUp()
         {
-            
+
+        }
+
+        static void EnsureDatabaseExists(string databaseName)
+        {
+            const string masterConnectionString = "server=.; database=master; trusted_connection=true";
+
+            using (var connection = new SqlConnection(masterConnectionString))
+            {
+                connection.Open();
+
+                try
+                {
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = $"CREATE DATABASE [{databaseName}]";
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (SqlException sqlException) when (sqlException.Number == DatabaseAlreadyExists)
+                {
+                }
+            }
         }
     }
 }
